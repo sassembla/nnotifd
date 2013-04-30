f84f4aa97d3e3db8a51ceb69cf7203259b27a3c3
sassembla <toru.inoue@kissaki.tv>
2013-04-30 17:26:01 +0900

    テスト追記

---
+++ nnotifd/nnotifd.h
@@ -0,0 +1,13 @@
+//
+//  nnotifd.h
+//  nnotifd
+//
+//  Created by sassembla on 2013/04/29.
+//  Copyright (c) 2013年 KISSAKI Inc,. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface nnotifd : NSApplication
+
+@end
---
+++ nnotifd/nnotifd.m
@@ -0,0 +1,13 @@
+//
+//  nnotifd.m
+//  nnotifd
+//
+//  Created by sassembla on 2013/04/29.
+//  Copyright (c) 2013年 KISSAKI Inc,. All rights reserved.
+//
+
+#import "nnotifd.h"
+
+@implementation nnotifd
+
+@end
